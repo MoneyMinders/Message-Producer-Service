d03bff7d28262c1e7b2c67b6273ca7f0e737e13e
Sahil Gakhar <sahil.gakhar@ringcentral.com>
2023-07-09 19:40:28 +0530

    Handling server exit gracefully

--- rest/controller/router.go
+++ rest/controller/router.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/mux"
 )
@@ -18,12 +19,34 @@ func RouterActor(ctx context.Context) error {
 	// Define the routes
 	router.HandleFunc(baseRouteProcessMessage+"/process-message", handleProcessMessageRequest).Methods("POST")
 
-	// Start the server with the router
-	log.Println("Server listening on :8080")
-	err := http.ListenAndServe(":8080", router)
+	// Create a new HTTP server with the router
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: router,
+	}
+
+	// Start the server in a separate goroutine
+	go func() {
+		log.Println("Server listening on :8080")
+		err := server.ListenAndServe()
+		if err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Failed to start server: %v", err)
+		}
+	}()
+
+	// Wait for the cancellation signal from the context
+	<-ctx.Done()
+
+	// Shutdown the server gracefully
+	log.Println("Shutting down server...")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	err := server.Shutdown(ctx)
 	if err != nil {
-		log.Fatalf("Failed to start server: %v", err)
+		log.Printf("Failed to gracefully shutdown server: %v", err)
 		return err
 	}
+
 	return nil
 }
+
